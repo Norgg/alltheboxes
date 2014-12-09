6520d993c0685ac53ee3f965f2e4c84fbc96523c
John Turner <j.turner@mhnltd.co.uk>
2014-12-09 18:27:51 +0000

    Start of python server impl.

---
+++ python/server.py
@@ -0,0 +1,51 @@
+import json
+
+from client import Client
+
+from editor import Editor
+
+
+from tornado.httpserver import HTTPServer
+from tornado.web import Application, RequestHandler, StaticFileHandler
+from tornado.websocket import WebSocketHandler
+
+
+class Index(RequestHandler):
+    def get(self):
+        with open('../js/client/index.html') as index:
+            self.write(index.read())
+
+
+class ClientConnection(WebSocketHandler):
+    def open(self):
+        self.application.server.clients.append(self)
+        self.client = Client(self)
+        self.editor = Editor(self)
+
+    def on_message(self, message):
+        data = json.loads(message)
+        self.client.on_message(data)
+
+    def on_close(self):
+        self.application.server.clients.remove(self)
+
+
+class Server(object):
+    def __init__(self, world, port):
+        self.application = Application([
+            (r'/', Index),
+            (r'/ws', ClientConnection),
+            (r'/(.*)', StaticFileHandler, {'path': '../js/client/'}),
+        ])
+        self.application.server = self
+
+        self.world = world
+        self.port = port
+        self.http_server = HTTPServer(self.application)
+        self.clients = []
+
+    def listen(self):
+        self.http_server.listen(self.port)
+
+    def send_update(self):
+        pass
