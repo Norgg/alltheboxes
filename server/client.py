5be3f60d285c90a58704acaddd2f2bd4c86b1167
John Turner <j.turner@mhnltd.co.uk>
2014-12-12 12:39:27 +0000

    Remove old server dir.

--- server/client.py
+++
@@ -1,6 +0,0 @@
-class Client(object):
-    def __init__(self, connection):
-        self.connection = connection
-
-    def on_message(self, message):
-        print("Client message: {}".format(message))
--- server/editor.py
+++
@@ -1,6 +0,0 @@
-class Editor(object):
-    def __init__(self, connection):
-        self.connection = connection
-
-    def on_message(self, message):
-        print("Editor message: {}".format(message))
