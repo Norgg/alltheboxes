2f70f09eef0351d18a7bddda0179d0000b028ab6
John Turner <j.turner@mhnltd.co.uk>
2014-12-18 15:26:26 +0000

    Add missing persisted.py file.  Client websocket changes that weren't included in previous commit.

--- client/client.js
+++ client/client.js
@@ -1,185 +1,199 @@
 var ClientMethods = {
-  addOutput: function(data) {
-    console.log(data);
-    var msg = "";
-    
-    if (data.user) {
-      msg = '~<span class="name">'+escapeHTML(data.user)+"</span>: ";
-    }
-    if (data.joined) {
-      msg = '<span class="roomname">'+escapeHTML(data.joined)+"</span>\n";
-    }
-    if (data.contents) {
-      this.setContents(data.contents);
-    }
-    
-    if (data.text) {
-      msg += escapeHTML(data.text)+"\n";
-      this.output.append(msg);
-      
-      var text = this.output.text();
-      if (text.length > this.bufSize) this.output.text(text.slice(-this.bufSize));
-      window.localStorage.log = this.output.html();
-      
-      this.output.animate({scrollTop: this.output[0].scrollHeight}, 50);
-      
-      this.resize();
+    addOutput: function(data) {
+        console.log(data);
+        var msg = "";
 
-      this.output.linkify();
-    }
-
-    if (!document.hasFocus()) document.title = "*alltheboxes";
-  },
-
-  resize: function() {
-    var maxHeight = $(window).height() - this.input.height() - this.contents.height();
-    this.output.height('auto');
-    if (this.output.height() > maxHeight) {
-      this.output.height(maxHeight);
-      this.output.animate({scrollTop: this.output[0].scrollHeight}, 10);
-    }
-  },
-
-  keyup: function(evt) {
-    var inputText = this.input.val();
-    if (inputText[0] == "/") {
-      this.input.addClass("cmd");
-    } else {
-      this.input.removeClass("cmd");
-    }
-  },
-
-  setID: function(data) {
-    //console.log("Storing room: " + data);
-    $.cookie('id', data);
-  },
-
-  setContents: function(data) {
-    var self = this;
-    this.contents.empty();
-    $.each(data, function(idx, elem) {
-      var li = $('<li>');
-      li.text(elem.name);
-      li.attr('title', elem.description);
-      self.contents.append(li);
-    });
-  },
-
-  onConnect: function(evt) {
-    //TODO: Move this onto the server and have it save and replay recent history per location if there's an item there recording it.
-    this.output.html(window.localStorage.log.slice(-this.bufSize));
-    this.addOutput({text: "Logging in at " + new Date().toUTCString()});
-    
-    this.socket.emit('login', $.cookie('id'));
-  },
-  
-  onDisconnect: function(evt) {
-    this.addOutput({ text: "Disconnected at " + new Date().toUTCString() + ", attempting to reconnect...\n" });
-  },
-
-  keydown: function(evt) {
-    var inputText = this.input.val();
-    if (evt.keyCode == 13) { //return
-      evt.preventDefault();
-      if (!inputText) return;
-
-      if (inputText != this.history[0]) {
-        this.history.unshift(inputText);
-        while (this.history.length > this.maxHistory) {
-          this.history.pop();
+        if (data.user) {
+            msg = '~<span class="name">'+escapeHTML(data.user)+"</span>: ";
         }
-      }
+        if (data.joined) {
+            msg = '<span class="roomname">'+escapeHTML(data.joined)+"</span>\n";
+        }
+        if (data.contents) {
+            this.setContents(data.contents);
+        }
 
-      if (inputText[0] == "/") {
-        inputText = inputText.slice(1);
-        this.socket.emit('cmd', inputText);
-        this.input.val("/");
-      } else {
-        this.socket.emit('chat', inputText);
-        this.input.val("");
-      }
+        if (data.text) {
+            msg += escapeHTML(data.text)+"\n";
+            this.output.append(msg);
 
-      this.historyIdx = -1;
-      return false;
-    } else if (evt.keyCode == 9) { //tab
-      evt.preventDefault();
-      
-      if (inputText[0] == "/") {
-        inputText = inputText.slice(1);
-        this.input.addClass("cmd");
-      } else {
-        inputText = "/" + inputText;
-        this.input.removeClass("cmd");
-      }
+            var text = this.output.text();
+            if (text.length > this.bufSize) this.output.text(text.slice(-this.bufSize));
+            window.localStorage.log = this.output.html();
 
-      this.input.val(inputText);
+            this.output.animate({scrollTop: this.output[0].scrollHeight}, 50);
 
-      return false;
-    } else if (evt.keyCode == 38) { //up arrow
-      if (this.historyIdx == -1) this.originalInput = inputText;
-      this.historyIdx++;
-      if (this.history[this.historyIdx] != undefined) {
-        evt.preventDefault();
-        this.input.val(this.history[this.historyIdx]);
-      } else {
-        this.historyIdx--;
-      }
-    } else if (evt.keyCode == 40) { //down arrow
-      this.historyIdx--;
-      if (this.history[this.historyIdx]) {
-        evt.preventDefault();
-        this.input.val(this.history[this.historyIdx]);
-      } else {
-        this.input.val(this.originalInput);
-        this.historyIdx = -1;
-      }
-    }
-  },
+            this.resize();
+
+            this.output.linkify();
+        }
+
+        if (!document.hasFocus()) document.title = "*alltheboxes";
+    },
+
+    resize: function() {
+        var maxHeight = $(window).height() - this.input.height() - this.contents.height();
+        this.output.height('auto');
+        if (this.output.height() > maxHeight) {
+            this.output.height(maxHeight);
+            this.output.animate({scrollTop: this.output[0].scrollHeight}, 10);
+        }
+    },
+
+    keyup: function(evt) {
+        var inputText = this.input.val();
+        if (inputText[0] == "/") {
+            this.input.addClass("cmd");
+        } else {
+            this.input.removeClass("cmd");
+        }
+    },
+
+    setID: function(data) {
+        //console.log("Storing room: " + data);
+        $.cookie('id', data);
+    },
+
+    setContents: function(data) {
+        var self = this;
+        this.contents.empty();
+        $.each(data, function(idx, elem) {
+            var li = $('<li>');
+            li.text(elem.name);
+            li.attr('title', elem.description);
+            self.contents.append(li);
+        });
+    },
+
+    onConnect: function(evt) {
+        //TODO: Move this onto the server and have it save and replay recent history per location if there's an item there recording it.
+        this.output.html(window.localStorage.log.slice(-this.bufSize));
+        this.addOutput({text: "Logging in at " + new Date().toUTCString()});
+
+        this.emit({'login': $.cookie('id')});
+    },
+
+    emit: function(obj) {
+        this.socket.send(JSON.stringify(obj));
+    },
+
+    onDisconnect: function(evt) {
+        this.addOutput({ text: "Disconnected at " + new Date().toUTCString() + ", attempting to reconnect...\n" });
+    },
+
+    onMessage: function(evt) {
+        console.log(evt);
+        var msg = JSON.parse(evt.data);
+
+        if (msg['output']) {
+            this.addOutput({'text': msg['output']});
+        } else if (msg['refresh']) {
+            window.location.reload(true);
+        }
+    },
+
+    keydown: function(evt) {
+        var inputText = this.input.val();
+        if (evt.keyCode == 13) { //return
+            evt.preventDefault();
+            if (!inputText) return;
+
+            if (inputText != this.history[0]) {
+                this.history.unshift(inputText);
+                while (this.history.length > this.maxHistory) {
+                    this.history.pop();
+                }
+            }
+
+            if (inputText[0] == "/") {
+                inputText = inputText.slice(1);
+                this.emit({'cmd': inputText});
+                this.input.val("/");
+            } else {
+                this.emit({'chat': inputText});
+                this.input.val("");
+            }
+
+            this.historyIdx = -1;
+            return false;
+        } else if (evt.keyCode == 9) { //tab
+            evt.preventDefault();
+
+            if (inputText[0] == "/") {
+                inputText = inputText.slice(1);
+                this.input.addClass("cmd");
+            } else {
+                inputText = "/" + inputText;
+                this.input.removeClass("cmd");
+            }
+
+            this.input.val(inputText);
+
+            return false;
+        } else if (evt.keyCode == 38) { //up arrow
+            if (this.historyIdx == -1) this.originalInput = inputText;
+            this.historyIdx++;
+            if (this.history[this.historyIdx] != undefined) {
+                evt.preventDefault();
+                this.input.val(this.history[this.historyIdx]);
+            } else {
+                this.historyIdx--;
+            }
+        } else if (evt.keyCode == 40) { //down arrow
+            this.historyIdx--;
+            if (this.history[this.historyIdx]) {
+                evt.preventDefault();
+                this.input.val(this.history[this.historyIdx]);
+            } else {
+                this.input.val(this.originalInput);
+                this.historyIdx = -1;
+            }
+        }
+    },
 };
 
 var Client = function() {
-  var self=this;
-  
-  var url;
-  if (window.url) url = window.url;
-  else url = location.protocol+'//'+location.hostname+(location.port ? ':'+location.port: '');
-  this.socket = io.connect(url);
-  this.socket.on('output', function (data) { self.addOutput(data); });
-  this.socket.on('_id', function(data) { self.setID(data); });
-  this.socket.on('connect', function(evt) { self.onConnect(evt); });
-  this.socket.on('disconnect', function(evt) { self.onDisconnect(evt); });
-  this.socket.on('refresh', function(evt) { window.location.reload(true); });
+    var self=this;
 
-  this.bufSize = 1000000;
+    var url;
+    if (window.url) url = window.url;
+    else url = 'ws://'+location.hostname+(location.port ? ':'+location.port: '') + '/ws';
+    this.socket = new WebSocket(url);
 
-  this.cmdMode = false;
-  
-  this.input = $('#input');
-  this.output = $('#output');
-  this.contents = $('#contents');
-  this.contents.hide();
-  this.resize();
-  this.input.keyup(function(evt) {self.keyup(evt);});
-  this.input.keydown(function(evt) {self.keydown(evt);});
+    this.socket.onopen = function(evt) { self.onConnect(evt); };
+    this.socket.onclose = function(evt) { self.onDisconnect(evt); }
+    this.socket.onmessage = function(evt) { self.onMessage(evt); }
 
-  this.maxHistory = 500;
-  this.history = [];
-  this.historyIdx = 0;
-  this.originalInput = "";
+    this.bufSize = 1000000;
 
-  $(window).resize(function(evt) {self.resize();});
-  $(window).focus(function(evt) {document.title="alltheboxes";});
+    this.cmdMode = false;
 
-  $(this.input).blur(function(evt) {
-    function refocus() {if (!window.getSelection().toString().length) self.input.focus(); else setTimeout(refocus, 100)};
-    setTimeout(refocus, 100);
-  });
+    this.input = $('#input');
+    this.output = $('#output');
+    this.contents = $('#contents');
+    this.contents.hide();
+    this.resize();
+    this.input.keyup(function(evt) {self.keyup(evt);});
+    this.input.keydown(function(evt) {self.keydown(evt);});
 
-  $(window).on('copy', function(evt) {setTimeout(function() {self.input.focus();}, 100);});
+    this.maxHistory = 500;
+    this.history = [];
+    this.historyIdx = 0;
+    this.originalInput = "";
 
-  if (!window.localStorage.log) window.localStorage.log = "";
-  
-  $(window).on('unload', function(evt) { window.localStorage.log += "Goodbye at " + new Date().toUTCString() + "\n\n"; });
+    $(window).resize(function(evt) {self.resize();});
+    $(window).focus(function(evt) {document.title="alltheboxes";});
+
+    $(this.input).blur(function(evt) {
+        function refocus() {if (!window.getSelection().toString().length) self.input.focus(); else setTimeout(refocus, 100)};
+        setTimeout(refocus, 100);
+    });
+
+    $(window).on('copy', function(evt) {setTimeout(function() {self.input.focus();}, 100);});
+
+    if (!window.localStorage.log) window.localStorage.log = "";
+
+    $(window).on('unload', function(evt) { window.localStorage.log += "Goodbye at " + new Date().toUTCString() + "\n\n"; });
 };
 
 Client.prototype = ClientMethods;
---
+++ server/persisted.py
@@ -0,0 +1,32 @@
+from tornado.gen import coroutine
+
+
+class Persisted(object):
+    table = ""
+
+    def __init__(self, world, data):
+        self.world = world
+        self.data = data
+        self.id = data.get('id')
+
+    @coroutine
+    def save(self):
+        fields, values = zip(*self.data.items())
+        if self.id is None:
+            print("Creating {}".format(self.__class__.__name__))
+            fieldstr = ','.join(fields)
+            valstr = ','.join('%s' for _ in values)
+            create_query = 'insert into {} ({}) values ({}) returning *'.format(self.__class__.table, fieldstr, valstr)
+            result = yield self.world.db.query(create_query, values)
+            self.data = result.as_dict()
+            self.id = self.data.get('id')
+        else:
+            fieldstr = ','.join(['{} = %s'.format(field) for field in fields])
+            update_query = 'update {} set {} where id = {}'.format(self.__class__.table, fieldstr, self.id)
+            yield self.world.db.query(update_query, values)
+
+    def __getitem__(self, *args, **kwargs):
+        return self.data.__getitem__(*args, **kwargs)
+
+    def __setitem__(self, *args, **kwargs):
+        return self.data.__setitem__(*args, **kwargs)
