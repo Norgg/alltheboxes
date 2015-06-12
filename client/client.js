dcb6cb193f314dad7f6b8e0ea5f17fc3372da2f5
John Turner <j.turner@mhnltd.co.uk>
2015-06-12 21:55:06 +0100

    Move client and editor to new js style.

--- client/client.js
+++ client/client.js
@@ -1,7 +1,59 @@
-var ClientMethods = {
-    addOutput: function(lines) {
-        var self = this;
+var Client = function() {
+    var self = {};
+    
+    self.connected = false;
 
+    var url = 'ws://'+location.hostname+(location.port ? ':'+location.port: '') + '/ws';
+    self.socket = new WebSocket(url);
+
+    self.socket.onopen = function(evt) { self.onConnect(evt); };
+    self.socket.onclose = function(evt) { self.onDisconnect(evt); }
+    self.socket.onmessage = function(evt) { self.onMessage(evt); }
+
+    self.bufSize = 1000000;
+
+    self.cmdMode = false;
+
+    self.input = $('#input');
+    self.output = $('#output');
+    self.contents = $('#contents');
+    self.input.keyup(function(evt) {self.keyup(evt);});
+    self.input.keydown(function(evt) {self.keydown(evt);});
+
+    self.maxHistory = 500;
+    self.history = [];
+    self.historyIdx = 0;
+    self.originalInput = "";
+    self.reconnect = true;
+
+    $(window).resize(function(evt) {self.resize();});
+    $(window).focus(function(evt) {document.title="alltheboxes";});
+
+    $(self.input).blur(function(evt) {
+        function refocus() {
+            if (!window.getSelection().toString().length && !$('#styleSwap').is(':focus')) {
+                self.input.focus();
+            } else { 
+                setTimeout(refocus, 100);
+            }
+        };
+        setTimeout(refocus, 100);
+    });
+
+    $('#styleSwap').on('change', function(evt) {
+        $('#style')[0].href=self.value;
+    });
+
+    $(window).on('copy', function(evt) {setTimeout(function() {self.input.focus();}, 100);});
+
+    if (!window.localStorage.log) window.localStorage.log = "";
+
+    $(window).on('unload', function(evt) { 
+        self.addOutput([{ text: "Goodbye at " + new Date().toUTCString(), tags: ["goodbye"]}]);
+    });
+
+
+    self.addOutput = function(lines) {
         var wrapDiv = $('<div class="outputWrap"></div>');
         $.each(lines, function(idx, line) {
             console.log(lines);
@@ -30,74 +82,73 @@ var ClientMethods = {
         self.output.linkify();
 
         if (!document.hasFocus()) document.title = "*alltheboxes";
-    },
+    };
 
-    resize: function() {
-        var maxHeight = $(window).height() - this.input.height() - this.contents.height();
-        this.output.height('auto');
-        if (this.output.height() > maxHeight) {
-            this.output.height(maxHeight);
-            this.output.animate({scrollTop: this.output[0].scrollHeight}, 10);
+    self.resize = function() {
+        var maxHeight = $(window).height() - self.input.height() - self.contents.height();
+        self.output.height('auto');
+        if (self.output.height() > maxHeight) {
+            self.output.height(maxHeight);
+            self.output.animate({scrollTop: self.output[0].scrollHeight}, 10);
         }
-    },
+    };
 
-    keyup: function(evt) {
-        var inputText = this.input.val();
+    self.keyup = function(evt) {
+        var inputText = self.input.val();
         if (inputText[0] == "/") {
-            this.input.addClass("cmd");
+            self.input.addClass("cmd");
         } else {
-            this.input.removeClass("cmd");
+            self.input.removeClass("cmd");
         }
-    },
+    };
 
-    setToken: function(data) {
+    self.setToken = function(data) {
         //console.log("Storing room: " + data);
         $.cookie('token', data);
-    },
+    };
 
-    setContents: function(data) {
+    self.setContents = function(data) {
         console.log("Setting contents");
-        var self = this;
-        this.contents.empty();
+        self.contents.empty();
         $.each(data, function(idx, elem) {
             var li = $('<li>');
             li.text(elem.name);
             li.attr('title', elem.description);
             self.contents.append(li);
         });
-    },
+    };
 
-    onConnect: function(evt) {
-        //TODO: Move this onto the server and have it save and replay recent history per location if there's an item there recording it.
-        this.output.html(window.localStorage.log.slice(-this.bufSize));
-        this.addOutput([{text: "Logging in at " + new Date().toUTCString()}]);
+    self.onConnect = function(evt) {
+        //TODO: Move self onto the server and have it save and replay recent history per location if there's an item there recording it.
+        self.output.html(window.localStorage.log.slice(-self.bufSize));
+        self.addOutput([{text: "Logging in at " + new Date().toUTCString()}]);
 
-        this.connected = true;
+        self.connected = true;
 
         if ($.cookie('token')) {
-            this.emit({'login_token': $.cookie('token')});
+            self.emit({'login_token': $.cookie('token')});
         } else {
-            this.emit({'guest': true});
+            self.emit({'guest': true});
         }
-    },
+    };
 
-    emit: function(obj) {
-        this.socket.send(JSON.stringify(obj));
-    },
+    self.emit = function(obj) {
+        self.socket.send(JSON.stringify(obj));
+    };
 
-    onDisconnect: function(evt) {
-        self = this;
-        this.setContents([]);
-        if (this.connected) {
-            if (this.reconnect) {
-                this.addOutput([{text: "Disconnected at " + new Date().toUTCString() + ", attempting to reconnect...\n"}]);
+    self.onDisconnect = function(evt) {
+        self = self;
+        self.setContents([]);
+        if (self.connected) {
+            if (self.reconnect) {
+                self.addOutput([{text: "Disconnected at " + new Date().toUTCString() + ", attempting to reconnect...\n"}]);
             } else {
-                this.addOutput([{text: "Disconnected at " + new Date().toUTCString() + ".\n"}]);
+                self.addOutput([{text: "Disconnected at " + new Date().toUTCString() + ".\n"}]);
             }
         }
-        this.connected = false;
+        self.connected = false;
 
-        if (this.reconnect) {
+        if (self.reconnect) {
             setTimeout(function() {
                 console.log("Trying to reconnect.");
                 var url = 'ws://'+location.hostname+(location.port ? ':'+location.port: '') + '/ws';
@@ -108,9 +159,9 @@ var ClientMethods = {
                 self.socket.onmessage = function(evt) { self.onMessage(evt); }
             }, 1000);
         }
-    },
+    };
 
-    setStyles: function(styles) {
+    self.setStyles = function(styles) {
         var styleSwap = $('#styleSwap');
         styleSwap.empty();
         $.each(styles, function(i, style) {
@@ -120,137 +171,81 @@ var ClientMethods = {
           option.text(style);
           styleSwap.append(option);
         });
-    },
+    };
 
-    onMessage: function(evt) {
+    self.onMessage = function(evt) {
         console.log(evt);
         var msg = JSON.parse(evt.data);
         
-        if (msg.contents) this.setContents(msg.contents);
-        if (msg.output) this.addOutput(msg.output);
+        if (msg.contents) self.setContents(msg.contents);
+        if (msg.output) self.addOutput(msg.output);
         if (msg.refresh) window.location.reload(true);
-        if (msg.token) this.setToken(msg.token);
-        if (msg.styles) this.setStyles(msg.styles);
+        if (msg.token) self.setToken(msg.token);
+        if (msg.styles) self.setStyles(msg.styles);
         if (msg.disconnect) {
-            this.reconnect = false;
-            this.socket.close();
+            self.reconnect = false;
+            self.socket.close();
         }
-    },
+    };
 
-    keydown: function(evt) {
-        var inputText = this.input.val();
+    self.keydown = function(evt) {
+        var inputText = self.input.val();
         if (evt.keyCode == 13) { //return
             evt.preventDefault();
             if (!inputText) return;
 
-            if (inputText != this.history[0]) {
-                this.history.unshift(inputText);
-                while (this.history.length > this.maxHistory) {
-                    this.history.pop();
+            if (inputText != self.history[0]) {
+                self.history.unshift(inputText);
+                while (self.history.length > self.maxHistory) {
+                    self.history.pop();
                 }
             }
 
             if (inputText[0] == "/") {
                 inputText = inputText.slice(1);
-                this.emit({'cmd': inputText});
-                this.input.val("/");
+                self.emit({'cmd': inputText});
+                self.input.val("/");
             } else {
-                this.emit({'chat': inputText});
-                this.input.val("");
+                self.emit({'chat': inputText});
+                self.input.val("");
             }
 
-            this.historyIdx = -1;
+            self.historyIdx = -1;
             return false;
         } else if (evt.keyCode == 9) { //tab
             evt.preventDefault();
 
             if (inputText[0] == "/") {
                 inputText = inputText.slice(1);
-                this.input.addClass("cmd");
+                self.input.addClass("cmd");
             } else {
                 inputText = "/" + inputText;
-                this.input.removeClass("cmd");
+                self.input.removeClass("cmd");
             }
 
-            this.input.val(inputText);
+            self.input.val(inputText);
 
             return false;
         } else if (evt.keyCode == 38) { //up arrow
-            if (this.historyIdx == -1) this.originalInput = inputText;
-            this.historyIdx++;
-            if (this.history[this.historyIdx] != undefined) {
+            if (self.historyIdx == -1) self.originalInput = inputText;
+            self.historyIdx++;
+            if (self.history[self.historyIdx] != undefined) {
                 evt.preventDefault();
-                this.input.val(this.history[this.historyIdx]);
+                self.input.val(self.history[self.historyIdx]);
             } else {
-                this.historyIdx--;
+                self.historyIdx--;
             }
         } else if (evt.keyCode == 40) { //down arrow
-            this.historyIdx--;
-            if (this.history[this.historyIdx]) {
+            self.historyIdx--;
+            if (self.history[self.historyIdx]) {
                 evt.preventDefault();
-                this.input.val(this.history[this.historyIdx]);
+                self.input.val(self.history[self.historyIdx]);
             } else {
-                this.input.val(this.originalInput);
-                this.historyIdx = -1;
+                self.input.val(self.originalInput);
+                self.historyIdx = -1;
             }
         }
-    },
+    };
+    self.resize();
+    return self;
 };
-
-var Client = function() {
-    var self=this;
-    
-    this.connected = false;
-
-    var url = 'ws://'+location.hostname+(location.port ? ':'+location.port: '') + '/ws';
-    this.socket = new WebSocket(url);
-
-    this.socket.onopen = function(evt) { self.onConnect(evt); };
-    this.socket.onclose = function(evt) { self.onDisconnect(evt); }
-    this.socket.onmessage = function(evt) { self.onMessage(evt); }
-
-    this.bufSize = 1000000;
-
-    this.cmdMode = false;
-
-    this.input = $('#input');
-    this.output = $('#output');
-    this.contents = $('#contents');
-    this.resize();
-    this.input.keyup(function(evt) {self.keyup(evt);});
-    this.input.keydown(function(evt) {self.keydown(evt);});
-
-    this.maxHistory = 500;
-    this.history = [];
-    this.historyIdx = 0;
-    this.originalInput = "";
-    this.reconnect = true;
-
-    $(window).resize(function(evt) {self.resize();});
-    $(window).focus(function(evt) {document.title="alltheboxes";});
-
-    $(this.input).blur(function(evt) {
-        function refocus() {
-            if (!window.getSelection().toString().length && !$('#styleSwap').is(':focus')) {
-                self.input.focus();
-            } else { 
-                setTimeout(refocus, 100);
-            }
-        };
-        setTimeout(refocus, 100);
-    });
-
-    $('#styleSwap').on('change', function(evt) {
-        $('#style')[0].href=this.value;
-    });
-
-    $(window).on('copy', function(evt) {setTimeout(function() {self.input.focus();}, 100);});
-
-    if (!window.localStorage.log) window.localStorage.log = "";
-
-    $(window).on('unload', function(evt) { 
-        self.addOutput([{ text: "Goodbye at " + new Date().toUTCString(), tags: ["goodbye"]}]);
-    });
-};
-
-Client.prototype = ClientMethods;
--- client/editor.js
+++ client/editor.js
@@ -1,132 +1,127 @@
-var EditorMethods = {
-    onConnect: function(evt) {
-        this.editDiv.empty();
-        console.log("Connected");
-        this.emit({'getWorld': true});
-    },
-
-    onWorld: function(world) {
-        console.log("Got world.");
-        this.rooms = world.locations;
-        this.entities = world.entities;
-        this.roomEditors = {};
-        for (var roomId in this.rooms) {
-            var roomEditor = RoomEditor(this, this.rooms[roomId]);
-            this.roomEditors[roomId] = roomEditor;
-        }
-
-        this.entityEditors = {};
-        for (var entityId in this.entities) {
-            var entityEditor = EntityEditor(this, this.entities[entityId]);
-            this.entityEditors[entityId] = entityEditor;
-        }
-        this.drawLines();
-    },
-
-    onSaved: function(roomId) {
-        var self = this;
-        var roomEditor = this.roomEditors[roomId];
-        roomEditor.div.find('.roomContent').hide(200, function(){self.drawLines()});
-        roomEditor.refreshContent();
-    },
-
-    onDestroyed: function(roomId) {
-        this.roomEditors[roomId].div.remove();
-        this.drawLines();
-    },
-
-    onUpdated: function(room) {
-        console.log("Got update for " + room.name);
-        var roomEditor = this.roomEditors[room.id];
-        roomEditor.data = room;
-        roomEditor.refreshContent();
-        roomEditor.div.find('.headTitle').text(room.name);
-        this.onMoved(room);
-    },
-
-    onMoved: function(room) {
-        var roomEditor = this.roomEditors[room.id];
-        roomEditor.moveTo(room.edit_x, room.edit_y);
-
-        this.drawLines();
-    },
-
-    onCreated: function(room) {
-        this.rooms[room.id] = room;
-        var roomEditor = RoomEditor(this, room);
-        this.roomEditors[room.id] = roomEditor;
-        roomEditor.makeRoomBox(this);
-        this.drawLines();
-    },
-
-    createRoom: function(evt) {
-        evt.preventDefault();
-        if (this.newRoomName.val()) this.emit({'createRoom': this.newRoomName.val()});
-        this.newRoomName.val("");
-    },
-    
-    createEntity: function(evt) {
-        evt.preventDefault();
-        if (this.newEntityName.val()) this.emit({'createEntity': this.newEntityName.val()});
-        this.newEntityName.val("");
-    },
-
-    resize: function(evt) {
-        console.log("resizing");
-        this.cvs.width($(document).width());
-        this.cvs.height($(document).height());
-        this.cvs.attr("width",    this.cvs.width());
-        this.cvs.attr("height", this.cvs.height());
-        this.drawLines();
-    },
-
-    drawLines: function() {
-        this.cvs[0].width = this.cvs[0].width;
-        //this.gfx.clearRect(0, 0, this.cvs.width(), this.cvs.height());
-        for (var roomId in this.roomEditors) {
-            this.roomEditors[roomId].exitLines();
-        }
-    },
-
-    onMessage: function(evt) {
-        var msg = JSON.parse(evt.data);
-        if (msg.world) this.onWorld(msg.world);
-        if (msg.roomSaved) this.onSaved(msg.roomSaved);
-        if (msg.roomDestroyed) this.onDestroyed(msg.roomDestroyed);
-        if (msg.roomCreated) this.onCreated(msg.roomCreated);
-        if (msg.roomUpdated) this.onUpdated(msg.roomUpdated);
-        if (msg.roomMoved) this.onMoved(msg.roomMoved);
-        if (msg.refresh) window.location.reload(true);
-    }, 
-
-    emit: function(obj) {
-        this.socket.send(JSON.stringify(obj));
-    }
-};
-
 var Editor = function() {
-    var self = this;
+    var self = {};
     self.maxZ = 1;
 
-    this.editDiv = $('#editor');
-    this.newRoomName = $('#newRoomName');
-    this.newEntityName = $('#newEntityName');
+    self.editDiv = $('#editor');
+    self.newRoomName = $('#newRoomName');
+    self.newEntityName = $('#newEntityName');
 
     var url = 'ws://'+location.hostname+(location.port ? ':'+location.port: '') + '/ws';
-    this.socket = new WebSocket(url);
-    this.socket.onopen = function(evt) { self.onConnect(evt); };
-    this.socket.onmessage = function(evt) { console.log(evt); self.onMessage(evt); };
+    self.socket = new WebSocket(url);
+    self.socket.onopen = function(evt) { self.onConnect(evt); };
+    self.socket.onmessage = function(evt) { console.log(evt); self.onMessage(evt); };
     $('#newRoomForm').submit(function(evt) { self.createRoom(evt); });
     $('#newEntityForm').submit(function(evt) { self.createEntity(evt); });
 
-    this.cvs = $('#lines');
-    this.gfx = $('#lines')[0].getContext("2d");
+    self.cvs = $('#lines');
+    self.gfx = $('#lines')[0].getContext("2d");
 
     $(document).scroll(function(evt) { self.resize(); });
     $(window).resize(function(evt)     { self.resize(); });
-    this.resize();
 
-    var url;
+    self.onConnect = function(evt) {
+        self.editDiv.empty();
+        console.log("Connected");
+        self.emit({'getWorld': true});
+    };
+
+    self.onWorld = function(world) {
+        console.log("Got world.");
+        self.rooms = world.locations;
+        self.entities = world.entities;
+        self.roomEditors = {};
+        for (var roomId in self.rooms) {
+            var roomEditor = RoomEditor(self, self.rooms[roomId]);
+            self.roomEditors[roomId] = roomEditor;
+        }
+
+        self.entityEditors = {};
+        for (var entityId in self.entities) {
+            var entityEditor = EntityEditor(self, self.entities[entityId]);
+            self.entityEditors[entityId] = entityEditor;
+        }
+        self.drawLines();
+    };
+
+    self.onSaved = function(roomId) {
+        var roomEditor = self.roomEditors[roomId];
+        roomEditor.div.find('.roomContent').hide(200, function(){self.drawLines()});
+        roomEditor.refreshContent();
+    };
+
+    self.onDestroyed = function(roomId) {
+        self.roomEditors[roomId].div.remove();
+        self.drawLines();
+    };
+
+    self.onUpdated = function(room) {
+        console.log("Got update for " + room.name);
+        var roomEditor = self.roomEditors[room.id];
+        roomEditor.data = room;
+        roomEditor.refreshContent();
+        roomEditor.div.find('.headTitle').text(room.name);
+        self.onMoved(room);
+    };
+
+    self.onMoved = function(room) {
+        var roomEditor = self.roomEditors[room.id];
+        roomEditor.moveTo(room.edit_x, room.edit_y);
+
+        self.drawLines();
+    };
+
+    self.onCreated = function(room) {
+        self.rooms[room.id] = room;
+        var roomEditor = RoomEditor(self, room);
+        self.roomEditors[room.id] = roomEditor;
+        roomEditor.makeRoomBox(self);
+        self.drawLines();
+    };
+
+    self.createRoom = function(evt) {
+        evt.preventDefault();
+        if (self.newRoomName.val()) self.emit({'createRoom': self.newRoomName.val()});
+        self.newRoomName.val("");
+    };
+    
+    self.createEntity = function(evt) {
+        evt.preventDefault();
+        if (self.newEntityName.val()) self.emit({'createEntity': self.newEntityName.val()});
+        self.newEntityName.val("");
+    };
+
+    self.resize = function(evt) {
+        console.log("resizing");
+        self.cvs.width($(document).width());
+        self.cvs.height($(document).height());
+        self.cvs.attr("width",    self.cvs.width());
+        self.cvs.attr("height", self.cvs.height());
+        self.drawLines();
+    };
+
+    self.drawLines = function() {
+        self.cvs[0].width = self.cvs[0].width;
+        //self.gfx.clearRect(0, 0, self.cvs.width(), self.cvs.height());
+        for (var roomId in self.roomEditors) {
+            self.roomEditors[roomId].exitLines();
+        }
+    };
+
+    self.onMessage = function(evt) {
+        var msg = JSON.parse(evt.data);
+        if (msg.world) self.onWorld(msg.world);
+        if (msg.roomSaved) self.onSaved(msg.roomSaved);
+        if (msg.roomDestroyed) self.onDestroyed(msg.roomDestroyed);
+        if (msg.roomCreated) self.onCreated(msg.roomCreated);
+        if (msg.roomUpdated) self.onUpdated(msg.roomUpdated);
+        if (msg.roomMoved) self.onMoved(msg.roomMoved);
+        if (msg.refresh) window.location.reload(true);
+    }; 
+
+    self.emit = function(obj) {
+        self.socket.send(JSON.stringify(obj));
+    }
+    
+    self.resize();
+    return self;
 };
-
-Editor.prototype = EditorMethods;
